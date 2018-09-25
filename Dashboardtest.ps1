80af8b7594002075c77d32994dcdb35c4cf9f67b
Chris Magnuson <hourglasssand@gmail.com>
2018-09-24 21:32:46 -0400

    Mature test dashboard

--- Dashboardtest.ps1
+++ Dashboardtest.ps1
@@ -2,23 +2,34 @@ $MicorosftOAuthApplicationID = Get-PasswordstatePassword -ID 5415
 $MicrosoftUDAuthenticationMethod = New-UDAuthenticationMethod -AppId $MicorosftOAuthApplicationID.UserName -AppSecret $MicorosftOAuthApplicationID.Password -Provider Microsoft
 $LoginPage = New-UDLoginPage -AuthenticationMethod $MicrosoftUDAuthenticationMethod
 
-$WebToPrintImageByOrderPage = New-UDPage -Name "Web to print image" -Content {
+$WebToPrintImageByOrderPageInput = New-UDPage -Name "Web to print image" -Content {
     New-UDInput -Title "Web to print image by order number" -Endpoint {
         param (
-            [Parameter(HelpMessage="Order Number")]
-            $OrderNumber
+            [Parameter(HelpMessage="ERP Order Number",Mandatory)]
+            $ERPOrderNumber
         )
-        New-UDInputAction -Content {
-            New-udinput -Title test2 -Endpoint {
-                param (
-                    [Parameter(HelpMessage="Testing help message")]
-                    $Test3
-                )
-                New-UDInputAction -Toast "Test3"
-            }
-        }
+        New-UDInputAction -RedirectUrl /order/$ERPOrderNumber
     }
 }
 
-$Dashboard = New-UDDashboard -Title Test -Pages @($WebToPrintImageByOrderPage) -LoginPage $LoginPage
+$WebToPrintImageByOrderPage = New-UDPage -Url "/order/:ERPOrderNumber" -Endpoint {
+    param (
+        [Parameter(HelpMessage="Order Number")]
+        $ERPOrderNumber
+    )
+    Set-CustomyzerModuleEnvironment -Name Production
+    New-UDTable -Title "Customyzer Approval Order Header" -Headers OrderID, ERPOrderNumber, WebOrderNumber, CustomerEmail, StatusID, CreatedDateUTC -Endpoint {
+        Get-CustomyzerApprovalOrder -ERPOrderNumber $ERPOrderNumber |
+        Out-UDTableData -Property OrderID, ERPOrderNumber, WebOrderNumber, CustomerEmail, StatusID, CreatedDateUTC
+    }
+
+    New-UDTable -Title "Customyzer Approval Order Header" -Headers OrderID, ERPOrderNumber, WebOrderNumber, CustomerEmail, StatusID, CreatedDateUTC -Endpoint {
+        Get-CustomyzerApprovalOrder -ERPOrderNumber $ERPOrderNumber |
+        Out-UDTableData -Property OrderID, ERPOrderNumber, WebOrderNumber, CustomerEmail, StatusID, CreatedDateUTC
+    }
+}
+
+
+
+$Dashboard = New-UDDashboard -Title Test -Pages @($WebToPrintImageByOrderPageInput, $WebToPrintImageByOrderPage) -LoginPage $LoginPage
 Start-UDDashboard -AutoReload -Dashboard $Dashboard -AllowHttpForLogin
